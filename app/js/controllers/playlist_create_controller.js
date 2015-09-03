3a1530174e06c9a514a9caaa4ff4cd91905e2610
Roger Saner <rsaner@deloitte.co.za>
2015-09-03 16:29:31 +0200

    Playlist controller returns ride types

--- app/js/controllers/playlist_create_controller.js
+++ app/js/controllers/playlist_create_controller.js
@@ -1,5 +1,9 @@
-angular.module("app").controller('PlaylistCreateController', function($scope, $location, AuthenticationService) {
+angular.module("app").controller('PlaylistCreateController', function($scope, $location, ApiService, AuthenticationService, $http) {
   $scope.title = "Add a Ride";
+
+  $http.get('/api/v1.0/ride_types').success(function(data) {
+    $scope.ride_types = data;
+  });
 
   var onLogoutSuccess = function(response) {
     $location.path('/login');
