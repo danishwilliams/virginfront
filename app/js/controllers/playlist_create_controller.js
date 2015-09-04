8d961a1321b2afe1a9ce2becc37e811b7e0d5e9f
Roger Saner <rsaner@deloitte.co.za>
2015-09-04 12:57:29 +0200

    Added a songs model to the playlist_create controller

--- app/js/controllers/playlist_create_controller.js
+++ app/js/controllers/playlist_create_controller.js
@@ -1,17 +1,43 @@
-angular.module("app").controller('PlaylistCreateController', function($scope, $location, ApiService, AuthenticationService, $http) {
-  $scope.title = "Add a Ride";
+angular.module("app").controller('PlaylistCreateController', function ($scope, $location, ApiService, AuthenticationService, $http) {
+    $scope.title = "Add a Ride";
 
-  // TODO: move the 0 into some kind of persistent state
-  $http.get('/api/v1.0/rides/0').success(function(data) {
-    $scope.goals = data.goals;
-    $scope.name = data.name;
-  });
+    // TODO: move the 0 into some kind of persistent state
+    $http.get('/api/v1.0/rides/0').success(function (data) {
+      $scope.goals = data.goals;
+      $scope.name = data.name;
+    });
 
-  var onLogoutSuccess = function(response) {
-    $location.path('/login');
-  };
+    $scope.songs = [
+      {
+        name: 'Black Magic',
+        artist: 'Little Mix',
+        genre: 'Pop',
+        bpm: '80',
+        time: '03:31'
+      },
+      {
+        name: 'How deep is your love',
+        artist: 'Calvin Harris',
+        genre: 'Alternative',
+        bpm: '80',
+        time: '03:32'
+      },
+      {
+        name: 'Too bad, so sad',
+        artist: 'Matric',
+        genre: 'R&B/Soul',
+        bpm: '80',
+        time: '03:24'
+      }
+    ];
 
-  $scope.logout = function() {
-    AuthenticationService.logout().success(onLogoutSuccess);
-  };
-});
+    var onLogoutSuccess = function (response) {
+      $location.path('/login');
+    };
+
+    $scope.logout = function () {
+      AuthenticationService.logout().success(onLogoutSuccess);
+    };
+  }
+)
+;
