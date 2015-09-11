b931b0b9a47dccb6abea94507808c703f8d546eb
Roger Saner <rsaner@deloitte.co.za>
2015-09-11 15:37:39 +0200

    First pass at an ApiService

--- app/js/services/api_service.js
+++ app/js/services/api_service.js
@@ -1,3 +1,15 @@
 /**
  * Created by rogersaner on 15/09/03.
  */
+angular.module("app").factory('ApiService', function($http) {
+  var api = 'config/stubs/';
+  var version = 'v1.0';
+
+  // these routes map to stubbed API endpoints in config/server.js
+  return {
+    getRideTypes: function() {
+      console.log('getRideTypes()');
+      return $http.get(api + version + '/ride_types');
+    }
+  };
+});
