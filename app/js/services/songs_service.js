a71678b52a8aa368c6653bb019bc2862173e2f0d
Roger Saner <rsaner@deloitte.co.za>
2015-09-11 15:25:49 +0200

    Added album and source to song structure

--- app/js/services/songs_service.js
+++ app/js/services/songs_service.js
@@ -8,27 +8,33 @@ angular.module("app").factory('SongsService', function () {
       id: 100,
       name: 'Black Magic',
       artist: 'Little Mix',
+      album: 'Salute',
       genre: 'Pop',
-      bpm: '80',
-      time: '03:31'
+      bpm: 141,
+      time: 193,
+      source: 'https://cdn.example.com/path/to/track.mp3'
     },
     {
       id: 150,
       name: 'How deep is your love',
       artist: 'Calvin Harris',
       genre: 'Alternative',
-      bpm: '80',
-      time: '03:32'
+      album: 'Salute',
+      bpm: 80,
+      time: 233,
+      source: 'https://cdn.example.com/path/to/track.mp3'
     },
     {
       id: 175,
       name: 'Too bad, so sad',
       artist: 'Matric',
+      album: 'Salute',
       genre: 'R&B/Soul',
-      bpm: '80',
-      time: '03:24'
-      }
-    ];
+      bpm: 80,
+      time: 245,
+      source: 'https://cdn.example.com/path/to/track.mp3'
+    }
+  ];
 
   return {
     getSongs: function () {
