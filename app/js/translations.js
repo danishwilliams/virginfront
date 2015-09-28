b74ee15a5e1986cf007a116077b4260ce1c2da9a
Roger Saner <rsaner@deloitte.co.za>
2015-09-28 12:48:34 +0200

    First set of translations defined

---
+++ app/js/translations.js
@@ -0,0 +1,57 @@
+angular
+  .module("app")
+  .config(['$translateProvider', function ($translateProvider) {
+  $translateProvider.translations('en', {
+    'TITLE': 'Hello',
+    'FOO': 'This is a paragraph',
+
+    // Navigation
+    'BACK': 'Back',
+
+    // Playlists
+    'RIDE_TEMPLATE': 'Ride template',
+    'PLAYLIST_CREATE': 'Create playlist',
+    'PLAYLIST_REVIEW': 'Review',
+    'PLAYLIST_PUBLISH': 'Publish to device',
+    'CLASS_GIVE_NAME': 'Give your class a name...',
+    'DRAG_DROP_TRACK': 'Drag and drop your track here',
+    'PLAY': 'Play',
+    'BPM': 'BPM',
+    'RPM': 'RPM',
+    'LEG_SPEED': 'Leg speed',
+    'ADD_NOTES': 'Add more notes',
+    'CHOOSE_TRACK': 'Choose a track for',
+    'SEARCH': 'search',
+    'TRACKS': 'songs',
+    'SHOWING': 'showing',
+    'ADD_FILTER': 'Add filter'
+  });
+ 
+  $translateProvider.translations('es', {
+    'TITLE': 'Hola',
+    'FOO': 'Este es un párrafo',
+
+    // Navigation
+    'BACK': 'Atrás',
+
+    // Playlists
+    'RIDE_TEMPLATE': 'Plantilla Ride',
+    'PLAYLIST_CREATE': 'Crear lista de reproducción',
+    'PLAYLIST_REVIEW': 'Reseña',
+    'PLAYLIST_PUBLISH': 'Publicar a dispositivo',
+    'CLASS_GIVE_NAME': 'Dale a tu clase un nombre',
+    'DRAG_DROP_TRACK': 'Arrastre y suelte la pista aquíhere',
+    'PLAY': 'Jugar',
+    'BPM': 'BPM',
+    'RPM': 'RPM',
+    'LEG_SPEED': 'Velocidad de las piernas',
+    'ADD_NOTES': 'Añadir más notas',
+    'CHOOSE_TRACK': 'Elija una pista para',
+    'SEARCH': 'buscar',
+    'TRACKS': 'canciones',
+    'SHOWING': 'demostración',
+    'ADD_FILTER': 'Añadir filtro'
+  });
+ 
+  $translateProvider.preferredLanguage('en');
+}]);
