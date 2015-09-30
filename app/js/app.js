b19ae6e596968ed8da01cbe01611ab85b08528c0
Roger Saner <rsaner@deloitte.co.za>
2015-09-30 13:48:48 +0200

    Moved Restangular config into separate file. Added UUID generator

--- app/js/app.js
+++ app/js/app.js
@@ -3,13 +3,15 @@ angular
     "ngResource",
     "ngSanitize",
     "ngNewRouter",
+    "angularUUID2",
     "app.login",
     "app.goals",
     "app.playlist",
     "pascalprecht.translate",
-    "restangular"])
+    "restangular"
+  ])
   .controller("AppController", ['$router', AppController])
-  .config(function ($componentLoaderProvider, RestangularProvider) {
+  .config(function ($componentLoaderProvider) {
     /*
      * overriding the template mapping of the new router to make it
      * compatible with grunt-angular-templates
@@ -18,64 +20,20 @@ angular
       var dashName = dashCase(name);
       return '../js/components/' + dashName + '/' + dashName + '.html';
     });
-
-    /*
-     * Restangular config
-     */
-     //set the base url for api calls on our RESTful services
-    var newBaseUrl = "";
-    if (window.location.hostname === "localhost") {
-      newBaseUrl = "http://localhost:8000/varockstar/api/"; // Dane's laptop
-      newBaseUrl = "http://localhost:8000/api/1.0/"; // dev
-    } else {
-      var deployedAt = window.location.href.substring(0, window.location.href);
-      newBaseUrl = deployedAt + "/api/rest/register";
-    }
-    RestangularProvider.setBaseUrl(newBaseUrl);
-
-    RestangularProvider.setDefaultRequestParams({apikey: "secret key"});
-
-    RestangularProvider.addResponseInterceptor(function(data, operation, what, url, response, deferred) {
-      var extractedData;
-      console.log('Restangular operation: ' + operation + ', on ' + what + ' at ' + url);
-      // .. to look for getList operations
-      if (operation === "getList") {
-        // .. and handle the data and meta data
-
-        //data.message = 'An error has occurred.';
-
-        if (data.message === 'An error has occurred.') {
-          console.log('[ERROR] API down! Reverting to local fallback');
-          console.log(url);
-          console.log(what);
-
-          /*
-          Can't inject $http at this stage so probably move the entire function into .run
-          $http.get('/api/1.0/' . what).then(function(data) {
-            console.log(data);
-            extractedData = data.data;
-          });
-          */
-
-        }
-        else {
-          extractedData = data.ResponseObject;
-          extractedData.Success = data.Success;
-          extractedData.FailureMessage = data.FailureMessage;
-          extractedData.FriendlyFailureMessage = data.FriendlyFailureMessage;
-        }
-      } else {
-        extractedData = data.data;
-      }
-      return extractedData;
-    });
   });
 
 function AppController($router) {
-  $router.config([
-    {path: '/', redirectTo: '/login'},
-    {path: '/login', component: 'login'},
-    {path: '/admin/goals', component: 'goals'},
-    {path: '/playlist-create', component: 'playlist'},
-  ]);
+  $router.config([{
+    path: '/',
+    redirectTo: '/login'
+  }, {
+    path: '/login',
+    component: 'login'
+  }, {
+    path: '/admin/goals',
+    component: 'goals'
+  }, {
+    path: '/playlist-create',
+    component: 'playlist'
+  }, ]);
 }
---
+++ app/js/restangular.config.js
@@ -0,0 +1,98 @@
+angular.module("app").config(function (RestangularProvider) {
+  /*
+   * Restangular config
+   */
+  //set the base url for api calls on our RESTful services
+  var newBaseUrl = "";
+  if (window.location.hostname === "localhost") {
+    newBaseUrl = "http://localhost:8000/varockstar/api/"; // Dane's laptop, via ../config/proxy.js
+    //newBaseUrl = "http://localhost:8000/api/1.0/"; // dev
+  } else {
+    var deployedAt = window.location.href.substring(0, window.location.href);
+    newBaseUrl = deployedAt + "/api/rest/register";
+  }
+  RestangularProvider.setBaseUrl(newBaseUrl);
+
+  // The API's default id is Id with CamelCase
+  RestangularProvider.setRestangularFields({
+    id: "Id"
+  });
+
+  // Add a secret key as URL parameters into every request
+  RestangularProvider.setDefaultRequestParams({
+    apikey: "secret key"
+  });
+
+  // DELETEs are sent without a body
+  RestangularProvider.setRequestInterceptor(function(elem, operation) {
+    if (operation === "remove") {
+       return null;
+    }
+    return elem;
+  });
+
+  // Requests specify that we want JSON
+  RestangularProvider.addFullRequestInterceptor(function (element, operation, route, url, headers, params) {
+    if (operation === 'put' && route === 'goals') {
+      //console.log(element);
+      //console.log(params);
+      //element = 
+    }
+    return {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+  });
+
+  // TODO: figure out how to retry requests
+
+  RestangularProvider.addResponseInterceptor(function (data, operation, what, url, response, deferred) {
+    var extractedData;
+    console.log('Restangular operation: ' + operation + ', on ' + what + ' at ' + url);
+
+    if (data.Message === 'An error has occurred.') {
+      console.log('[ERROR] API down!');
+      return '';
+
+      /*
+      Can't inject $http at this stage so probably move the entire function into .run
+      $http.get('/api/1.0/' . what).then(function(data) {
+        console.log(data);
+        extractedData = data.data;
+      });
+      */
+
+    }
+
+    // .. to look for getList operations
+    if (operation === "getList") {
+      // .. and handle the data and meta data
+      extractedData = data.ResponseObject;
+      extractedData.Success = data.Success;
+      extractedData.FailureMessage = data.FailureMessage;
+      extractedData.FriendlyFailureMessage = data.FriendlyFailureMessage;
+    } else {
+      extractedData = data.data;
+    }
+    return extractedData;
+  });
+
+  RestangularProvider.setErrorInterceptor(function (response, deferred, responseHandler) {
+    if (response.status === 500) {
+      console.log('[ERROR] Internal Server Error');
+    }
+    if (response.status === 403) {
+      refreshAccesstoken().then(function () {
+        // Repeat the request and then call the handlers the usual way.
+        $http(response.config).then(responseHandler, deferred.reject);
+        // Be aware that no request interceptors are called this way.
+      });
+
+      return false; // error handled
+    }
+
+    return true; // error not handled
+  });
+
+});
