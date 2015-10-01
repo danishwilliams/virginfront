ed3980bf5cbd9e8c9b19800eb4ea3b6030fcda47
Roger Saner <rsaner@deloitte.co.za>
2015-10-01 12:43:19 +0200

    e2e playlist test refactored to work with UUIDs

--- spec-e2e/playlist_spec.js
+++ spec-e2e/playlist_spec.js
@@ -1,9 +1,24 @@
 describe('Create playlist', function () {
 
   var playlist;
-  var goalPlaylist;
-  var tracksList;
-  var tracks;
+  var goalPlaylist = [];
+  var tracksList = [];
+  var tracks = [];
+  var goalPlaylistBuilt = false;
+
+  /**
+   * Given some kind of collection (like playlistController.goals) return the member with SortOrder = sortorder
+   */
+  var returnItemBySortOrder = function(collection, sortorder) {
+    var value = {};
+    var found = false;
+    collection.forEach(function(item) {
+      if (!found && item.SortOrder === sortorder) {
+        value = item;
+      }
+    });
+    return value;
+  };
 
   beforeEach(function () {
     playlist = element.all(by.repeater('goal in playlist.goals'));
@@ -15,17 +30,32 @@ describe('Create playlist', function () {
     tracksList = element.all(by.repeater('track in playlist.tracks'));
     element.all(by.repeater('track in playlist.tracks')).then(function (rows) {
       tracks = rows;
+
+      // Add track name and BPM value into the track array for easy access later
+      var i = 0;
+      var j = 0;
+      tracks.forEach(function (value) {
+        // TODO: Refactor to use Array#some http://stackoverflow.com/questions/2641347/how-to-short-circuit-array-foreach-like-calling-break
+        value.element(by.binding('track.bpm')).getText().then(function (text) {
+          tracks[i].bpm = parseInt(text.substr(0, text.length - 4));
+          i++;
+        });
+        value.element(by.binding('track.name')).getText().then(function (text) {
+          tracks[j].trackName = text;
+          j++;
+        });
+      });
     });
   });
 
   describe('Goals', function () {
 
     it('Creates an empty structure for a playlist', function () {
-      expect(playlist.count()).toEqual(10);
+      expect(goalPlaylist.length).toEqual(11);
     });
 
     it('The first goal\'s name is "Warm Up"', function () {
-      expect(goalPlaylist[0].element(by.binding('goal.goal')).getText()).toEqual('Warm Up');
+      expect(goalPlaylist[0].element(by.binding('goal.Name')).getText()).toEqual('Warm Up');
     });
 
     it('The first goal should be selected by default', function () {
@@ -47,28 +77,40 @@ describe('Create playlist', function () {
     });
 
     it('Clicking on the second goal should select that goal and deselect the first goal', function () {
-      goalPlaylist[1].element(by.binding('goal.goal')).click();
+      goalPlaylist[1].element(by.binding('goal.Name')).click();
       expect(goalPlaylist[0].element(by.css('.goal-container')).getAttribute('class')).not.toMatch('active');
       expect(goalPlaylist[1].element(by.css('.goal-container')).getAttribute('class')).toMatch('active');
     });
 
-    it('There should be some tracks from Deezer', function () {
-      // Don't need to do this once existing Track objects are updated via callbacks; currently it's sloooooow
-      browser.sleep(3000);
-
+    it('There should be some tracks', function () {
       expect(tracksList.count()).toBeGreaterThan(0);
     });
 
     it('A track can be added to the second goal', function () {
-      tracks[4].element(by.css('.add')).click();
-      expect(goalPlaylist[1].element(by.css('.track-name')).getText()).toEqual('King');
+      var found = false;
+      var bpm = 0;
+      var i = 0;
+      var trackName = '';
+
+      tracks.forEach(function (value) {
+        if (!found && value.bpm >= 100 && value.bpm <= 120) {
+          //console.log('A track with acceptable BPM was found!');
+          found = true;
+          trackName = value.trackName;
+          value.element(by.css('.add')).click();
+        }
+      });
+
+      expect(found).toBe(true);
+
+      expect(goalPlaylist[1].element(by.css('.track-name')).getText()).toEqual(trackName);
     });
 
     it('A track outside of a goal\'s BPM range can\'t be added to that goal', function () {
-      goalPlaylist[0].element(by.binding('goal.goal')).click();
+      goalPlaylist[0].element(by.binding('goal.Name')).click();
       tracks[0].element(by.css('.add')).click();
 
-      goalPlaylist[0].all(by.binding('track.name')).then(function(items) {
+      goalPlaylist[0].all(by.binding('track.name')).then(function (items) {
         expect(items.length).toBe(0);
       });
     });
@@ -76,7 +118,7 @@ describe('Create playlist', function () {
     it('A track can be removed from a goal', function () {
       goalPlaylist[1].element(by.css('.add')).click();
 
-      goalPlaylist[0].all(by.binding('track.name')).then(function(items) {
+      goalPlaylist[0].all(by.binding('track.name')).then(function (items) {
         expect(items.length).toBe(0);
       });
     });
