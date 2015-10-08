2ba9be145db6de373a24435a78b0edb756061ac0
Roger Saner <rsaner@deloitte.co.za>
2015-10-08 15:53:16 +0200

    More promise documentation

--- app/js/components/templates/templates.controller.js
+++ app/js/components/templates/templates.controller.js
@@ -11,6 +11,7 @@ angular.module("app.templates", []).controller('TemplatesController', function (
         // TODO: there *must* be a way of updating an object within an array rather than searching through it by ID
         // There is!!!! https://www.jonathanfielding.com/combining-promises-angular/
         // Nah - that's a pattern for making multiple API calls which don't depend on each other
+        // Also see http://blog.xebia.com/2014/02/23/promises-and-design-patterns-in-angularjs/
         var found = false;
         _.mapObject(self.templateGroups, function (val, key) {
           if (!found && val.Id === data.TemplateGroupId) {
