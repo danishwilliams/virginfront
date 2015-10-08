98db261423fd1f73f825dfe301a1b59841fb6e2e
Roger Saner <rsaner@deloitte.co.za>
2015-10-08 15:46:01 +0200

    Extended Template Factory to be able to fetch template groups

--- app/js/components/templates/templates.controller.js
+++ app/js/components/templates/templates.controller.js
@@ -1,18 +1,34 @@
 angular.module("app.templates", []).controller('TemplatesController', function (Templates) {
   var self = this;
-  this.templates = Templates.getTemplates();
-  this.newtemplate = {};
+  this.templateGroups = Templates.getTemplateGroups();
+  this.newTemplateGroup = {};
 
-  Templates.loadTemplates(true).then(function (data) {
-    self.templates = data;
+  Templates.loadTemplateGroups(true).then(function (templateGroups) {
+    self.templateGroups = templateGroups;
+
+    templateGroups.forEach(function (val) {
+      Templates.loadTemplateGroupClasses(val.Id).then(function (data) {
+        // TODO: there *must* be a way of updating an object within an array rather than searching through it by ID
+        // There is!!!! https://www.jonathanfielding.com/combining-promises-angular/
+        var found = false;
+        _.mapObject(self.templateGroups, function (val, key) {
+          if (!found && val.Id === data.TemplateGroupId) {
+            found = true;
+            val.ClassLengths = data.TemplateClassLength;
+          }
+          return val;
+        });
+      });
+    });
   });
 
-  this.update = function (template) {
-    template.put();
+  this.updateTemplateGroup = function (templateGroup) {
+    templateGroup.put();
   };
 
-  this.delete = function (template) {
+  this.deleteTemplateGroup = function (template) {
     // TODO: fill this out
+    console.log("This is a really bad idea! I won't do it!");
   };
 
   this.create = function () {
--- app/js/components/templates/templates.factory.js
+++ app/js/components/templates/templates.factory.js
@@ -10,11 +10,16 @@ TemplatesFactory.$inject = ['Restangular'];
 function TemplatesFactory(Restangular) {
   var self = this;
   var templates = [];
+  var templateGroups = [];
 
   var templatesFactory = {
     loadTemplates: loadTemplates,
     getTemplates: getTemplates,
-    loadTemplate: loadTemplate
+    loadTemplate: loadTemplate,
+    loadTemplateGroups: loadTemplateGroups,
+    getTemplateGroups: getTemplateGroups,
+    loadTemplateGroup: loadTemplateGroup,
+    loadTemplateGroupClasses: loadTemplateGroupClasses
   };
 
   return templatesFactory;
@@ -44,4 +49,34 @@ function TemplatesFactory(Restangular) {
     }
   }
 
+  function loadTemplateGroups(includeGoals) {
+    return Restangular.all('templategroups').getList({
+      includeGoals: includeGoals
+    }).then(loadTemplateGroupsComplete);
+
+    function loadTemplateGroupsComplete(data, status, headers, config) {
+      self.templateGroups = data;
+      return self.templateGroups;
+    }
+  }
+
+  function getTemplateGroups() {
+    return templateGroups;
+  }
+
+  function loadTemplateGroup(id) {
+    return Restangular.one('templategroups', id).get().then(loadTemplateGroupComplete);
+
+    function loadTemplateGroupComplete(data, status, headers, config) {
+      return data;
+    }
+  }
+
+  function loadTemplateGroupClasses(id) {
+    return Restangular.one('templategroups/classlengthoptions', id).get().then(loadTemplateGroupClassesComplete);
+
+    function loadTemplateGroupClassesComplete(data, status, headers, config) {
+      return data;
+    }
+  }
 }
