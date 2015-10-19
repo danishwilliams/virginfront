d77be1cacdb11fe6119f039fe28cc618e10440e5
Roger Saner <rsaner@deloitte.co.za>
2015-10-19 18:22:55 +0200

    Added the rest of the onboarding skeleton

--- app/js/components/onboarding/onboarding.controller.js
+++ app/js/components/onboarding/onboarding.controller.js
@@ -1,6 +1,18 @@
-angular.module("app.onboarding", []).controller('OnboardingController', function ($stateParams) {
+angular.module("app.onboarding", []).controller('OnboardingController', function ($stateParams, $state, Genres) {
   var self = this;
   this.title = "Onboarding";
 
   this.id = $stateParams.id;
+
+  if ($state.current.name === 'onboarding-genres') {
+    Genres.loadGenres().then(function (data) {
+      self.genres = data;
+    });
+  }
+
+  this.save_password = function () {
+    $state.go('onboarding-clubs', {
+      id: self.id
+    });
+  };
 });
