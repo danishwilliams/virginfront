bd9e8b2f51caff58d6c593b31ef5fe9109e8c653
Roger Saner <rsaner@deloitte.co.za>
2015-10-19 12:24:35 +0200

    Small bug fix in Genre controller

--- app/js/components/genres/genres.controller.js
+++ app/js/components/genres/genres.controller.js
@@ -1,7 +1,6 @@
 angular.module("app.genres", []).controller('GenresController', function (Genres) {
   var self = this;
   this.title = "Genres";
-  this.id = $state.id;
 
   // TODO: bug fix for "Controller loads twice" @see https://github.com/angular/router/issues/204
   if (!self.genres) {
