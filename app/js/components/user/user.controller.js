670a981c8be1afa46d6c6be4f2137d2b1cacb217
Roger Saner <rsaner@deloitte.co.za>
2015-10-26 16:04:07 +0200

    Users can now edit their own gyms

--- app/js/components/user/user.controller.js
+++ app/js/components/user/user.controller.js
@@ -1,4 +1,4 @@
-angular.module("app.user", []).controller('UserController', function($stateParams, Users, Genres) {
+angular.module("app.user", []).controller('UserController', function($stateParams, Users, Genres, Gyms) {
   var self = this;
   this.title = "User profile";
   this.id = $stateParams.id;
@@ -8,6 +8,41 @@ angular.module("app.user", []).controller('UserController', function($stateParam
       self.user = data;
     });
   }
+
+// Load all gyms
+  this.loadGyms = function() {
+    Gyms.loadGyms().then(function(data) {
+      self.gymEdit = true;
+      self.gyms = data;
+      // Mark the user gyms which have been chosen
+      _.mapObject(self.gyms, function(val, key) {
+        if (key >= 0) {
+          var item = _.find(self.user.UserGyms, function(item) {
+            return item.Gym.Name === val.Name;
+          });
+          if (item) {
+            val.selected = true;
+          }
+        }
+        return val;
+      });
+    });
+  };
+
+  // Save gyms
+  this.saveGyms = function() {
+    // Replace the gyms in the userobject for saving
+    self.user.UserGyms = [];
+    self.gyms.forEach(function(val) {
+      if (val.selected) {
+        self.user.UserGyms.push({
+          Gym: val,
+          GymId: val.Id
+        });
+      }
+    });
+    self.update(self.user, 'gyms');
+  };
 
   // Load all genres
   this.loadGenres = function() {
@@ -50,6 +85,9 @@ angular.module("app.user", []).controller('UserController', function($stateParam
       switch (type) {
         case 'genres':
           self.genreEdit = false;
+          break;
+        case 'gyms':
+          self.gymEdit = false;
           break;
       }
     });
