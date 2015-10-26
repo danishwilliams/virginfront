15ff3d741892d3bc8121df03611953e847715d12
Roger Saner <rsaner@deloitte.co.za>
2015-10-26 15:51:40 +0200

    Loading up the current user from /api/users/me instead of hard-coding the id

--- app/js/components/users/users.factory.js
+++ app/js/components/users/users.factory.js
@@ -7,7 +7,7 @@ UsersFactory.$inject = ['Restangular'];
 function UsersFactory(Restangular) {
   var self = this;
   self.users = [];
-  self.currentUser = {Id: '04ee7331-dc30-4cc9-af6b-f4dc0877968d'}; // TODO: this is temporary
+  self.currentUser = {};
 
   var usersFactory = {
     loadUsers: loadUsers,
@@ -42,8 +42,7 @@ function UsersFactory(Restangular) {
 
   function loadCurrentUser() {
     // TODO: this is temporary
-    var id = '04ee7331-dc30-4cc9-af6b-f4dc0877968d';
-    return Restangular.one('users', id).get().then(loadCurrentUserComplete);
+    return Restangular.one('users/me').get().then(loadCurrentUserComplete);
 
     function loadCurrentUserComplete(data, status, headers, config) {
       self.currentUser = data;
