7758db3474e1030598d1fc77b5d1b6c3ba9cb048
Roger Saner <rsaner@deloitte.co.za>
2015-10-28 17:51:45 +0200

    Now showing user's clubs on their dashboard

--- app/js/components/dashboard/dashboard.controller.js
+++ app/js/components/dashboard/dashboard.controller.js
@@ -1,9 +1,13 @@
-angular.module("app.dashboard", []).controller('DashboardController', function (Playlists) {
+angular.module("app.dashboard", []).controller('DashboardController', function (Playlists, Users) {
   var self = this;
   this.title = "Dashboard";
 
   Playlists.loadPlaylists().then(function(data) {
     self.playlists = data;
+  });
+
+  Users.loadCurrentUser().then(function(data) {
+    self.gyms = data.UserGyms;
   });
 
 });
