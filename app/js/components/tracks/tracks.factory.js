8be7b78600ce71a095c0ff500037a846e883edd4
Roger Saner <rsaner@deloitte.co.za>
2015-11-03 11:40:26 +0200

    Removed unused loadUser from Tracks.factory.js

--- app/js/components/tracks/tracks.factory.js
+++ app/js/components/tracks/tracks.factory.js
@@ -10,8 +10,7 @@ function TracksComponentFactory(Restangular) {
 
   var tracksFactory = {
     loadTracks: loadTracks,
-    getTracks: getTracks,
-    loadUser: loadUser
+    getTracks: getTracks
   };
 
   return tracksFactory;
@@ -27,13 +26,5 @@ function TracksComponentFactory(Restangular) {
 
   function getTracks() {
     return tracks;
-  }
-
-  function loadUser(id) {
-    return Restangular.one('tracks', id).get().then(loadUserComplete);
-
-    function loadUserComplete(data, status, headers, config) {
-      return data;
-    }
   }
 }
