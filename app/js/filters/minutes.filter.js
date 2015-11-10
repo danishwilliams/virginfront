cb7fb0c4aafc1e840bd13f7d9e2e63e3a0518d80
Roger Saner <rsaner@deloitte.co.za>
2015-11-10 21:15:28 +0200

    minute filter returning a default value if no input is given

--- app/js/filters/minutes.filter.js
+++ app/js/filters/minutes.filter.js
@@ -5,6 +5,9 @@ angular
 // Converts seconds to minutes + seconds
 function SecondsToMinutesFilter() {
   return function (input) {
+    if (!input) {
+      return '00:00';
+    }
     function z(n) {
       return (n < 10 ? '0' : '') + n;
     }
