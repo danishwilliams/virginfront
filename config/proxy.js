6155210216f53d70088842e25214d5f80caa446d
Roger Saner <rsaner@deloitte.co.za>
2015-11-11 17:29:39 +0200

    Noted which proxy user is linked to which music provider account

--- config/proxy.js
+++ config/proxy.js
@@ -24,8 +24,8 @@ function onRequest(client_req, client_res) {
   options.path = client_req.url;
   options.method = client_req.method;
   options.headers = client_req.headers;
-  //var base64 = new Buffer('roger:Therodge321').toString('base64');
-  var base64 = new Buffer('dane:Therodge321').toString('base64');
+  //var base64 = new Buffer('roger:Therodge321').toString('base64'); // OpenEar
+  var base64 = new Buffer('dane:Therodge321').toString('base64'); // Simfy
   options.headers.Authorization = 'Basic ' + base64;
 
   var proxy = http.request(options, function (res) {
