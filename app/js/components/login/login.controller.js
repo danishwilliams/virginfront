8821d0457176eaaab82366b4b0875ae29ae46071
Roger Saner <rsaner@deloitte.co.za>
2015-12-10 16:39:14 +0200

    Added error handling for incorrect username/password

--- app/js/components/login/login.controller.js
+++ app/js/components/login/login.controller.js
@@ -5,23 +5,30 @@ LoginController.$inject = ['$location', '$stateParams', 'AuthenticationService',
 
 function LoginController($location, $stateParams, AuthenticationService, Users) {
   var self = this;
-  this.credentials = { username: "", password: "" };
+  this.credentials = {
+    username: "",
+    password: ""
+  };
   //b2c_login_check();
 
-  var onLoginSuccess = function() {
+  var onLoginSuccess = function () {
     console.log('onLoginSuccess');
     $location.path('/dashboard');
   };
 
-  this.login = function() {
+  this.login = function () {
     Users.setAuthHeader(self.credentials);
-    Users.loadCurrentUser().then(onLoginSuccess);
+    Users.loadCurrentUser().then(onLoginSuccess, function () {
+      self.error = {
+        error: true
+      };
+    });
   };
 
   // If a user has tried to log in via B2C, we get a #id_token value back
   function b2c_login_check() {
     var b2c_token = $location.hash();
-    if (b2c_token.substr(0,8) === 'id_token') {
+    if (b2c_token.substr(0, 8) === 'id_token') {
       console.log(b2c_token);
       b2c_token = b2c_token.substr(9);
       if (b2c_token.length > 10) {
