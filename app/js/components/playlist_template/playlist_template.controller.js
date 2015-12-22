94cab6055d369755caf88bf0650a83c72d22ab6f
Roger Saner <rsaner@deloitte.co.za>
2015-12-22 16:26:18 +0200

    Resetting a playlist object when creating a new playlist

--- app/js/components/playlist_template/playlist_template.controller.js
+++ app/js/components/playlist_template/playlist_template.controller.js
@@ -1,6 +1,7 @@
 angular.module("app.playlist_template", []).controller('Playlist_templateController', function (Playlists, Templates, spinnerService) {
   var self = this;
   Playlists.setStep(0);
+  Playlists.setPlaylist([]);
   this.title = "Choose a Ride Template";
 
   // TODO: For caching in Restangular, see http://makandracards.com/makandra/29143-angular-caching-api-responses-in-restangular
