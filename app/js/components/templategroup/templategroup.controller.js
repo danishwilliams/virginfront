580e6fef4568c516486029745b9bc08d806fdeda
Roger Saner <rsaner@deloitte.co.za>
2016-01-18 16:04:36 +0200

    Better form handling when saving a templategroup

--- app/js/components/templategroup/templategroup.controller.js
+++ app/js/components/templategroup/templategroup.controller.js
@@ -1,12 +1,24 @@
-angular.module("app.templategroup_view", []).controller('Templategroup_viewController', function ($stateParams, Templates) {
+angular.module("app.templategroup_view", []).controller('Templategroup_viewController', function ($stateParams, $state, Templates, spinnerService) {
   var self = this;
   this.id = $stateParams.id;
   this.templategroup = {};
 
-  console.log(this.id);
+  self.title = 'Create new template';
 
-  Templates.loadTemplateGroup(this.id).then(function(data) {
+  Templates.loadTemplateGroup(this.id).then(function (data) {
+    self.title = 'Edit template';
     self.templategroup = data;
+
+    Templates.loadTemplateGroupClasses(self.id).then(function (data) {
+      self.templategroup.ClassLengths = data.TemplateClassLength;
+    });
   });
 
+  self.saveTemplate = function() {
+    spinnerService.show('saveTemplateSpinner');
+    self.templategroup.put().then(function() {
+      spinnerService.hide('saveTemplateSpinner');
+      $state.go('templates-admin');
+    });
+  };
 });
