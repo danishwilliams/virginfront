0db2192e90b49eaa71e12c4581b4f0da02f575b4
Roger Saner <rsaner@deloitte.co.za>
2016-01-19 14:31:33 +0200

    Template time saving works! Now displaying Beat ratio too

--- app/js/components/template/template.controller.js
+++ app/js/components/template/template.controller.js
@@ -1,12 +1,23 @@
-angular.module("app.template", []).controller('TemplateController', function ($stateParams, Templates) {
+angular.module("app.template", []).controller('TemplateController', function ($stateParams, Templates, Beats, spinnerService) {
   var self = this;
   this.id = $stateParams.id;
   this.template = {};
 
-  $scope.effortOptions = [40, 50, 60, 70, 80, 90, 100];
+  self.effortOptions = [40, 50, 60, 70, 80, 90, 100];
+  self.bpmOptions = [60, 70, 80, 90, 100, 110, 120, 130, 140];
 
   Templates.loadTemplate(this.id).then(function (data) {
     self.template = data;
   });
 
+  Beats.loadBeats().then(function(data) {
+    self.beats = data;
+  });
+
+  self.saveTemplate = function() {
+    spinnerService.show('saveTemplateTimeSpinner');
+    self.template.put().then(function() {
+      spinnerService.hide('saveTemplateTimeSpinner');
+    });
+  };
 });
