78d85baac011db70b2f8388c650bee3de06b6395
Roger Saner <rsaner@deloitte.co.za>
2016-01-22 09:59:53 +0200

    Simple icon select for templates

--- app/js/components/templategroup/templategroup.controller.js
+++ app/js/components/templategroup/templategroup.controller.js
@@ -3,18 +3,36 @@ angular.module("app.templategroup_view", []).controller('Templategroup_viewContr
   this.id = $stateParams.id;
   this.templategroup = {};
 
-  Templates.loadTemplateGroup(this.id).then(function (data) {
-    self.templategroup = data;
-    spinnerService.hide('loadTemplateGroupSpinner');
+  if ($state.current.name === 'templategroup') {
+    // Viewing/editing a template group
+    Templates.loadTemplateGroup(this.id).then(function (data) {
+      self.templategroup = data;
+      spinnerService.hide('loadTemplateGroupSpinner');
 
-    Templates.loadTemplateGroupClasses(self.id).then(function (data) {
-      spinnerService.hide('loadClassLengthsSpinner');
-      self.templategroup.ClassLengths = data.TemplateClassLength;
+      Templates.loadTemplateGroupClasses(self.id).then(function (data) {
+        spinnerService.hide('loadClassLengthsSpinner');
+        self.templategroup.ClassLengths = data.TemplateClassLength;
+      });
     });
-  });
+  } else {
+    // Creating a template group
+    self.templategroup = Templates.createBlankTemplateGroup();
+  }
+
+  self.iconFileNames = [
+    '',
+    'allterrain.svg',
+    'freestyle.svg',
+    'hills.svg',
+    'intervals.svg',
+    'pack.svg',
+    'speed.svg',
+    'strengthendurance.svg'
+  ];
 
   self.saveTemplate = function () {
     spinnerService.show('saveTemplateSpinner');
+    console.log(self.templategroup);
     self.templategroup.put().then(function () {
       spinnerService.hide('saveTemplateSpinner');
       $state.go('templates-admin');
@@ -28,17 +46,17 @@ angular.module("app.templategroup_view", []).controller('Templategroup_viewContr
     });
   };
 
-  self.archiveTemplateGroup = function (){
+  self.archiveTemplateGroup = function () {
     spinnerService.show('archiveTemplateGroupSpinner');
-    Templates.disableTemplateGroup(self.templategroup.Id).then(function(data) {
+    Templates.disableTemplateGroup(self.templategroup.Id).then(function (data) {
       spinnerService.hide('archiveTemplateGroupSpinner');
       self.templategroup.Enabled = false;
     });
   };
 
-  self.unArchiveTemplateGroup = function (){
+  self.unArchiveTemplateGroup = function () {
     spinnerService.show('archiveTemplateGroupSpinner');
-    Templates.enableTemplateGroup(self.templategroup.Id).then(function(data) {
+    Templates.enableTemplateGroup(self.templategroup.Id).then(function (data) {
       spinnerService.hide('archiveTemplateGroupSpinner');
       self.templategroup.Enabled = true;
     });
