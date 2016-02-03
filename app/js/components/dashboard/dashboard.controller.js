f14c86cca4777ccd8ebb7014221c8a39ad065715
Roger Saner <rsaner@deloitte.co.za>
2016-02-03 12:58:59 +0200

    Playlist sync info reloads every 15 seconds so we can see sync status

--- app/js/components/dashboard/dashboard.controller.js
+++ app/js/components/dashboard/dashboard.controller.js
@@ -1,4 +1,4 @@
-angular.module("app.dashboard", []).controller('DashboardController', function (Playlists, spinnerService) {
+angular.module("app.dashboard", []).controller('DashboardController', function (Playlists, spinnerService, $interval, $scope) {
   var self = this;
   this.title = "Dashboard";
 
@@ -9,8 +9,22 @@ angular.module("app.dashboard", []).controller('DashboardController', function (
   });
 
   Playlists.loadGymsPlaylistSyncInfoDetailed().then(function (data) {
-    self.gyms = data;
     spinnerService.hide('dashboardGymsSpinner');
+    self.gyms = data;
+  });
+
+  // Should probably put this into a gyms_rides directive (which then calls the gym_rides directive) so that
+  // we don't have to inject $scope into this controller. Urgh.
+  var intervalPromise = $interval(function () {
+    Playlists.loadGymsPlaylistSyncInfoDetailed().then(function (data) {
+      self.gyms = data;
+    });
+  }, 15000);
+
+  // Cancel the interval when navigating away from the page
+  // @see http://stackoverflow.com/questions/21364480/in-angular-how-to-use-cancel-an-interval-on-user-events-like-page-change
+  $scope.$on('$destroy', function () {
+    $interval.cancel(intervalPromise);
   });
 
   Playlists.loadRecentClasses(4).then(function (data) {
