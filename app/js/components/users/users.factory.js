72bdba9d1523e0b0b339949cbd18ea9c2d651e1c
Roger Saner <rsaner@deloitte.co.za>
2016-02-10 12:17:42 +0200

    Slight change to auth object for manual users/me auth

--- app/js/components/users/users.factory.js
+++ app/js/components/users/users.factory.js
@@ -94,7 +94,7 @@ function UsersFactory(Restangular, Storage) {
   function loadCurrentUser(token) {
     // If there is a token, we're manually passing this through because this is the onboarding first login
     if (token) {
-      return Restangular.one('users/me').get({}, {'Authorization': 'Token ' + token}).then(loadCurrentUserComplete);
+      return Restangular.one('users/me').get({}, {Authorization: 'Token ' + token}).then(loadCurrentUserComplete);
     }
     return Restangular.one('users/me').get().then(loadCurrentUserComplete);
 
