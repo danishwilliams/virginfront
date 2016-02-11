dff3892e2e2f2b682c21f19d3d06aefabb8493c4
Roger Saner <rsaner@deloitte.co.za>
2016-02-11 16:12:46 +0200

    Added error handling and a spinner when creating a user

--- app/js/components/user_new/user_new.controller.js
+++ app/js/components/user_new/user_new.controller.js
@@ -1,7 +1,9 @@
-angular.module("app.user_new", []).controller('UserNewController', function (Users, UserTypes, Gyms, Restangular, $state) {
+angular.module("app.user_new", []).controller('UserNewController', function (Users, UserTypes, Gyms, Restangular, $state, spinnerService) {
   var self = this;
 
-  self.newUser = {};
+  self.newUser = {
+    UserUserTypes: []
+  };
 
   UserTypes.loadUserTypes().then(function (data) {
     self.userTypes = data;
@@ -22,7 +24,6 @@ angular.module("app.user_new", []).controller('UserNewController', function (Use
   // If City, use that for Location. If no City and a Gym, use the Gym's location
 
   self.createUser = function () {
-    console.log(self.newUser);
     // Validation
     if (!self.newUser.FirstName || !self.newUser.LastName || !self.newUser.Email) {
       return;
@@ -33,14 +34,39 @@ angular.module("app.user_new", []).controller('UserNewController', function (Use
       return;
     }
 
-    // TODO: save the new user
+    // Add the user types which have been chosen in the UI
+    self.userTypes.forEach(function (val) {
+      if (val.selected) {
+        val.UserTypeId = val.Id;
+        val.Id = undefined;
+        val.UserType = {
+          Name: val.Name,
+          PublicRole: val.PublicRole
+        };
+        self.newUser.UserUserTypes.push(val);
+      }
+    });
+
+    spinnerService.show('creatingUser');
+    self.saving = true;
+
+    // Save the new user
     self.newUser.Username = self.newUser.Email;
-    Users.createNewUser(self.newUser).then(function() {
+    Users.createNewUser(self.newUser).then(function () {
+      spinnerService.hide('creatingUser');
+      self.saving = false;
       $state.go('users-admin');
+    }, function (res) {
+      spinnerService.hide('creatingUser');
+      self.saving = false;
+      self.serverError = {
+        error: true,
+        message: res.data.Message
+      };
     });
   };
 
-  self.hasLocation = function() {
+  self.hasLocation = function () {
     // If no City has been chosen, determine a location a Gym    
     var found = false;
     if (!self.newUser.LocationId) {
@@ -50,8 +76,7 @@ angular.module("app.user_new", []).controller('UserNewController', function (Use
           self.newUser.LocationId = val.LocationId;
         }
       });
-    }
-    else {
+    } else {
       found = true;
     }
     // If no location, show some error messaging
@@ -61,8 +86,7 @@ angular.module("app.user_new", []).controller('UserNewController', function (Use
       };
       console.log('no location found');
       return false;
-    }
-    else {
+    } else {
       self.error = {};
       return true;
     }
