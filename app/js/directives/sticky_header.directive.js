8162776bd145f0d98eb88ae33f5fb3ca0b93a8ca
Roger Saner <rsaner@deloitte.co.za>
2016-02-18 14:51:21 +0200

    Sticky header on playlists no longer weirdly jumps

--- app/js/directives/sticky_header.directive.js
+++ app/js/directives/sticky_header.directive.js
@@ -2,9 +2,9 @@ angular
   .module("app")
   .directive("stickyHeader", stickyHeader);
 
-stickyHeader.$inject = ['$window'];
+stickyHeader.$inject = ['$window', '$compile'];
 
-function stickyHeader($window) {
+function stickyHeader($window, $compile) {
   var directive = {
     restrict: 'A',
     link: link
@@ -16,21 +16,35 @@ function stickyHeader($window) {
     var rect = {};
     var offsetTop = 0; // get element's offset top relative to document
     var width = 0;
+    var node = {};
 
     angular.element(document).ready(function () {
       rect = element[0].getBoundingClientRect();
       offsetTop = rect.top; // get element's offset top relative to document
       width = rect.width;
+      height = rect.height;
+
+      // The placeholder element which mimicks the height of the actual element
+      node = document.createElement('div');
+      node.style.height = height + 'px';
     });
 
     $win.on('scroll', function (e) {
       if ($win[0].scrollY >= offsetTop) {
+        if (element.hasClass('fixed')) {
+          return;
+        }
         element.addClass('fixed');
         element.css({
-          width: width + 'px'
+          width: width + 'px',
         });
+        element[0].parentNode.firstElementChild.appendChild(node, this.firstElementChild);
       } else {
+        if (!element.hasClass('fixed')) {
+          return;
+        }
         element.removeClass('fixed');
+        element[0].parentNode.firstElementChild.removeChild(node, this.firstElementChild);
       }
     });
 
