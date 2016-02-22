b70cce9d5402497fe3e30eb6579069aa4cbeaadf
Roger Saner <rsaner@deloitte.co.za>
2016-02-22 10:19:23 +0200

    Adjusted copy for password reset screen

--- app/js/app/translations.config.js
+++ app/js/app/translations.config.js
@@ -47,6 +47,7 @@ angular.module("app").config(['$translateProvider', function ($translateProvider
     'PASSWORD_CONFIRM_REQ': 'Please confirm your password',
     'PASSWORDS_DONT_MATCH': "Your passwords don't match",
     'EMAIL_INVITE': 'Send email invitation',
+    'CHOOSE_NEW_PASSWORD': 'Choose a new password',
 
     // Container page
     'HOME': 'Home',
