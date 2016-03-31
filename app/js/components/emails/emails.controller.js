7deee764e94ca8f365c42394294f0ac46a383381
Roger Saner <rsaner@deloitte.co.za>
2016-03-31 16:24:49 +0200

    Improved the emails page by adding failures and the full email log

--- app/js/components/emails/emails.controller.js
+++ app/js/components/emails/emails.controller.js
@@ -1,7 +1,13 @@
 angular.module("app.emails", []).controller('EmailsController', function (Emails) {
   var self = this;
 
-  Emails.loadBounces().then(function(data) {
-    self.emails = data;
+  // @see https://sendgrid.com/docs/API_Reference/Webhooks/event.html for what the values mean
+
+  Emails.loadLogs(1, 10).then(function(data) {
+    self.logs = data;
+  });
+
+  Emails.loadFails(1, 10).then(function(data) {
+    self.fails = data;
   });
 });
