f5d0d441523365e4af30298d393243c4259982cf
Roger Saner <rsaner@deloitte.co.za>
2016-04-01 16:20:48 +0200

    Reworked the problematic emails part of the emails page

--- app/js/components/emails/emails.controller.js
+++ app/js/components/emails/emails.controller.js
@@ -9,5 +9,43 @@ angular.module("app.emails", []).controller('EmailsController', function (Emails
 
   Emails.loadFails(1, 10).then(function(data) {
     self.fails = data;
+    self.fails.forEach(function(entry) {
+      // Figure out the failure reason, date, and what action to take
+      if (entry.Deferred) {
+        entry.reason = 'EMAIL_DEFERRED';
+        entry.date = entry.DateDeferred;
+        entry.action = 'EMAIL_RESEND';
+      }
+      else if (entry.SpamReport) {
+        entry.reason = 'EMAIL_SPAM';
+        entry.date = entry.DateSpamReport;
+        entry.action = 'EMAIL_RESEND';
+      }
+      else if (entry.Bounce) {
+        if (entry.BounceReason) {
+          entry.reason = entry.BounceReason;
+        }
+        else {
+          entry.reason = 'EMAIL_INVALID';
+        }
+        entry.date = entry.DateBounce;
+        entry.action = 'EMAIL_EDIT';
+      }
+      else if (entry.Dropped) {
+        if (entry.DroppedReason) {
+          entry.reason = entry.DroppedReason;
+        }
+        else {
+          entry.reason = 'EMAIL_INVALID';
+        }
+        entry.date = entry.DateDropped;
+        entry.action = 'EMAIL_EDIT';
+      }
+      if (!entry.date) {
+        entry.date = entry.CreateDate;
+      }
+
+      // Figure out the failure date
+    });
   });
 });
