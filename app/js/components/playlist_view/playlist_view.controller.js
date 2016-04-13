cc38738b8b4f7ad08da5aa2eb39b1737351cd2e5
Roger Saner <rsaner@deloitte.co.za>
2016-04-13 10:56:05 +0200

    If playlist is incomplete, and it's my playlist, automatically go to the edit page

--- app/js/components/playlist_view/playlist_view.controller.js
+++ app/js/components/playlist_view/playlist_view.controller.js
@@ -19,8 +19,8 @@ angular.module("app.playlist_view", []).controller('Playlist_viewController', fu
     // Load an existing playlist
     Playlists.loadPlaylist(self.id).then(function () {
       self.playlist = Playlists.getPlaylist();
-      // If playlist is incomplete, edit it
-      if (!Playlists.checkAllGoalsHaveTracks()) {
+      // If playlist is incomplete, and it's my playlist, edit it
+      if (!self.playlist.Complete && self.user.Id === self.playlist.UserId) {
         $state.go('playlist-edit', {id: self.id});
       }
       spinnerService.hide('playlistViewSpinner');
