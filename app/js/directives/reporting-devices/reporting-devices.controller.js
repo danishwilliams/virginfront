c6101056574e423fc30781f33611e700da0ba2dc
Roger Saner <rsaner@deloitte.co.za>
2016-05-04 17:13:24 +0200

    Added spinners to the device boxes on the reporting dashboard

--- app/js/directives/reporting-devices/reporting-devices.controller.js
+++ app/js/directives/reporting-devices/reporting-devices.controller.js
@@ -12,12 +12,14 @@ function reportingDevices() {
   return directive;
 }
 
-reportingDevicesController.$inject = ['Devices', 'Reporting', 'spinnerService'];
+reportingDevicesController.$inject = ['Devices', 'spinnerService'];
 
-function reportingDevicesController(Devices, Reporting, spinnerService) {
+function reportingDevicesController(Devices, spinnerService) {
   var self = this;
 
   Devices.loadSyncStatus().then(function (data) {
+    spinnerService.hide('devicesSpinner');
+    spinnerService.hide('devices1Spinner');
     self.devices = {
       connected: 0,
       disconnected: 0,
@@ -46,75 +48,4 @@ function reportingDevicesController(Devices, Reporting, spinnerService) {
     });
     self.devicesLoaded = true;
   });
-
-  /*
-  // Load up registered/unregistered instructors
-  Reporting.loadRegisteredInstructors().then(function(data) {
-    self.registered = {
-      registered: 0,
-      unregistered: 0
-    };
-    data.forEach(function(val) {
-      if (val.UserState === 'registered') {
-        self.registered.registered = self.registered.registered + val.Count;
-      }
-      else {
-        self.registered.unregistered = self.registered.unregistered + val.Count;
-      }
-    });
-    if (self.registered.unregistered === 0) {
-      self.registered.percent = 100;
-    }
-    else {
-      self.registered.percent = Math.floor(self.registered.registered / (self.registered.registered + self.registered.unregistered) * 100);
-    }
-    spinnerService.hide('reportRegSpinner');
-  });
-
-  // Load up active/inactive instructors
-  Reporting.loadActiveInactiveInstructors(14).then(function(data) {
-    self.active = {
-      active: data.ActiveCount,
-      inactive: data.InactiveCount
-    };
-    if (self.active.inactive === 0) {
-      self.active.percent = 100;
-    }
-    else {
-      self.active.percent = Math.floor(self.active.active / (self.active.active + self.active.inactive) * 100);
-    }
-    spinnerService.hide('reportActiveSpinner');
-  });
-
-  // Load up email failures
-  Emails.loadFails(1, 10000).then(function(data) {
-    self.emailFailedCount = data.length;
-    self.emailLoaded = {loaded: true};
-    spinnerService.hide('reportEmailSpinner');
-  });
-
-  // Load up top templates
-  Reporting.loadTemplatesUsedInRides(14).then(function(data) {
-    self.templates = data;
-    spinnerService.hide('reportTemplatesSpinner');
-  });
-
-  // Load up top rides per club
-  Reporting.loadRidesTaughtPerClub(14).then(function(data) {
-    self.rides = data;
-    var top = 0;
-    // Find the highest value
-    self.rides.forEach(function(val) {
-      if (val.Count > top) {
-        top = val.Count;
-      }
-    });
-
-    // Use this as a baseline to figure out what percentage each ride is
-    self.rides.forEach(function(val) {
-      val.Percent = Math.floor(val.Count / top * 100);
-    });
-    spinnerService.hide('reportClubSpinner');
-  });
-  */
 }
