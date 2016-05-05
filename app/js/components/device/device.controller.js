8652d82733114a19190ed52b2e9be7198bac42fb
Roger Saner <rsaner@deloitte.co.za>
2016-05-05 14:37:47 +0200

    Added calls to sync log and heartbeat on the device page

--- app/js/components/device/device.controller.js
+++ app/js/components/device/device.controller.js
@@ -4,6 +4,19 @@ angular.module("app.device", []).controller('DeviceController', function ($state
 
   Devices.loadDevice(this.id).then(function (data) {
     self.device = data;
+    spinnerService.hide('device');
+  });
+
+  // Load the device sync log
+  Devices.loadDeviceSyncLog(self.id).then(function (data) {
+    self.synclog = data;
+    spinnerService.hide('synclog');
+  });
+
+  // Load the device heartbeat log
+  Devices.loadDeviceHeartbeatLog(self.id).then(function (data) {
+    self.heartbeatlog = data;
+    spinnerService.hide('heartbeatlog');
   });
 
   this.update = function (device) {
