4dd1d95a8fed3b8c60c550042d975acbeaf28fc1
Roger Saner <rsaner@deloitte.co.za>
2016-05-05 12:48:56 +0200

    Starting to get the device page loading up more stuff

--- app/js/components/device/device.controller.js
+++ app/js/components/device/device.controller.js
@@ -1,9 +1,23 @@
-angular.module("app.device", []).controller('DeviceController', function ($stateParams, Devices) {
+angular.module("app.device", []).controller('DeviceController', function ($stateParams, Devices, spinnerService) {
   var self = this;
   this.id = $stateParams.id;
 
+  // Load the device details
   Devices.loadDevice(this.id).then(function (data) {
     self.device = data;
+    spinnerService.hide('device');
+  });
+
+  // Load the device sync log
+  Devices.loadDeviceSyncLog(self.id).then(function (data) {
+    self.synclog = data;
+    spinnerService.hide('synclog');
+  });
+
+  // Load the device heartbeat log
+  Devices.loadDeviceHeartbeatLog(self.id).then(function (data) {
+    self.heartbeatlog = data;
+    spinnerService.hide('heartbeatlog');
   });
 
   this.update = function (device) {
