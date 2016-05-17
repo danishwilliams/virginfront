db7a67e4bf08582e3062c03ae5b37c0785e8943f
Roger Saner <rsaner@deloitte.co.za>
2016-05-17 15:45:45 +0200

    Changed how RPM is calculated for SA users - the Beat Ratio is calculated from the track's BPM range relative to the goal's BPM range

--- app/js/directives/goal_option/goal_option.directive.js
+++ app/js/directives/goal_option/goal_option.directive.js
@@ -14,11 +14,14 @@ function goalOption() {
       // This is because javascript is a pass-by-value language, and so primitives are copied within a nested scope.
       // @see http://zcourts.com/2013/05/31/angularjs-if-you-dont-have-a-dot-youre-doing-it-wrong/
       goaloption: '=',
-      bpm: '@',
+      trackBpm: '@', // BPM of the track - used to calculate the Beat Ratio
+      goalBpmLow: '@', // The lower BPM of the Goal's BPM range
+      goalBpmHigh: '@', // The higher BPM of the Goal's BPM range
       freestyle: '@',
       customrpm: '@',
-      'totalGoaloptions': '@', // Only show the Goal Option name if IsCustomRpm is false and there is more than 1 goaloption
-      'challengeGoal': '@' // If this is a challenge goal, pass the ID through (so that we can disable editing the metadata)
+      goalGoaloptionNumber: '@', // The number of goal options in this goal OMG that's not confusing at all
+      totalGoaloptions: '@', // Only show the Goal Option name if IsCustomRpm is false and there is more than 1 goaloption
+      challengeGoal: '@' // If this is a challenge goal, pass the ID through (so that we can disable editing the metadata)
     }
   };
   return directive;
@@ -27,6 +30,13 @@ function goalOption() {
 goalOptionController.$inject = ['$scope'];
 
 function goalOptionController($scope) {
+  // Because these should be integers. *sigh*
+  $scope.trackBpm = parseInt($scope.trackBpm);
+  $scope.goalBpmLow = parseInt($scope.goalBpmLow);
+  $scope.goalBpmHigh = parseInt($scope.goalBpmHigh);
+  $scope.totalGoaloptions = parseInt($scope.totalGoaloptions);
+  $scope.goalGoaloptionNumber = parseInt($scope.goalGoaloptionNumber);
+
   $scope.effortOptions = [40];
   for (i = 45; i <= 100; i = i + 5) {
     $scope.effortOptions.push(i);
@@ -49,28 +59,40 @@ function goalOptionController($scope) {
   }
 
   if ($scope.goaloption.Beat) {
-    switch ($scope.goaloption.Beat.Ratio) {
-      case 0.5:
-        $scope.beat = 'HALF_TIME';
-        break;
-      case 1:
-        $scope.beat = 'ON_THE_BEAT';
-        break;
+    // Figure out Beat Ratio based on chosen track
+
+    if (($scope.totalGoaloptions > 0 && $scope.goalGoaloptionNumber === 1)) {
+      half_time();
+    }
+    else if (($scope.goalBpmLow <= $scope.trackBpm && $scope.trackBpm <= $scope.goalBpmHigh)) {
+      // The track BPM is in the goal option's BPM range, it's "on the beat"
+      console.log($scope.goalBpmLow + ' <= ' + $scope.trackBpm + ' <= ' + $scope.goalBpmHigh, 'On the beat');
+      $scope.goaloption.Beat.Ratio = 1;
+      $scope.beat = 'ON_THE_BEAT';
+    }
+    else {
+      half_time();
     }
   }
 
+  function half_time() {
+    console.log($scope.goalBpmLow + ' <= ' + $scope.trackBpm + ' <= ' + $scope.goalBpmHigh, 'Half time');
+    $scope.goaloption.Beat.Ratio = 0.5;
+    $scope.beat = 'HALF_TIME';
+  }
+
   $scope.rpm = '';
-  if ($scope.bpm) {
+  if ($scope.trackBpm) {
     updateBpm();
   }
 
-  $scope.$watch('bpm', function () {
+  $scope.$watch('trackBpm', function () {
     updateBpm();
   });
 
   function updateBpm() {
     if ($scope.customrpm === 'false' && $scope.goaloption.Beat) {
-      $scope.rpm = parseInt($scope.bpm * $scope.goaloption.Beat.Ratio);
+      $scope.rpm = parseInt($scope.trackBpm * $scope.goaloption.Beat.Ratio);
     }
   }
 
