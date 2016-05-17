30552ad9df6e71c1d9389b07f7d9a3e627b65208
Roger Saner <rsaner@deloitte.co.za>
2016-05-17 13:00:21 +0200

    When disabling a device, if there's an error, show it.

--- app/js/components/device_edit/device_edit.controller.js
+++ app/js/components/device_edit/device_edit.controller.js
@@ -22,7 +22,25 @@ angular.module("app.device_edit", []).controller('DeviceEditController', functio
     self.action = 'disable';
     self.device.Enabled = self.device.Primary = false;
     self.saving = true;
-    saveDevice(self.newPrimary);
+    spinnerService.show('saveDeviceSpinner');
+    Devices.disableDevice(self.device.Id).then(function(data) {
+      saveDevice(self.newPrimary);
+    }, function(err) {
+      self.saved = true;
+      self.saving = false;
+
+      spinnerService.hide('saveDeviceSpinner');
+
+      self.alert = {
+        type: 'warning',
+        msg: 'DEVICE_DISABLE_FAILED'
+      };
+
+      // Either a generic error or a Music Provider error
+      if (err.data.Message.startsWith('Error while cancelling device music provider account')) {
+        msg = 'MUSIC_PROVIDER_CANCELLING_ERROR';
+      }
+    });
   };
 
   this.saveDevice = function () {
@@ -86,6 +104,9 @@ angular.module("app.device_edit", []).controller('DeviceEditController', functio
       type: 'success',
       msg: 'DEVICE_SAVED'
     };
+    if (self.action === 'disable') {
+      alert.msg = 'DEVICE_DISABLE_SUCCESS';
+    }
   }
 
   function saveError() {
@@ -95,5 +116,8 @@ angular.module("app.device_edit", []).controller('DeviceEditController', functio
       type: 'warning',
       msg: 'DEVICE_SAVE_ERROR'
     };
+    if (self.action === 'disable') {
+      alert.msg = 'DEVICE_DISABLE_FAILED';
+    }
   }
 });
