93d929ede67ed8ea16764bbdb05d61936d0c0d8b
Roger Saner <rsaner@deloitte.co.za>
2016-05-18 18:43:21 +0200

    Added a heatbeat log for Music Providers

--- app/js/components/music_providers/music_providers.controller.js
+++ app/js/components/music_providers/music_providers.controller.js
@@ -1,7 +1,23 @@
-angular.module("app.music_providers", []).controller('Music_providersController', function (MusicProviders) {
+angular.module("app.music_providers", []).controller('Music_providersController', function (Users, MusicProviders, Heartbeat, spinnerService) {
   var self = this;
 
-  MusicProviders.loadMusicProviders().then(function(data) {
-    self.music_providers = data;
+  var user = Users.getCurrentUser();
+  var id = user.Location.Country.MusicProvider.Id;
+  self.name = user.Location.Country.MusicProvider.Name;
+
+  MusicProviders.getHeartbeatLog(id, 288, 1).then(function (data) {
+    var log = Heartbeat.createHeartbeat(data);
+    self.heartbeat = log.heartbeat;
+    self.hasHeartbeat = log.hasHeartbeat;
+    self.newDay = log.newDay;
+    self.newDayIndex = log.newDayIndex;
+    spinnerService.hide('heartbeatlog');
   });
+
+  self.popoverContents = function (beat) {
+    if (beat.beat) {
+      return 'CONNECTED';
+    }
+    return 'DISCONNECTED';
+  };
 });
