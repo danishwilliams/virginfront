3f19222f2cf48c934185099ad034ce7f1526920b
Roger Saner <rsaner@deloitte.co.za>
2016-05-19 13:17:04 +0200

    Removed some unused logic for calculating each day in a Music Provider heartbeat

--- app/js/components/music_providers/music_providers.controller.js
+++ app/js/components/music_providers/music_providers.controller.js
@@ -20,45 +20,6 @@ angular.module("app.music_providers", []).controller('Music_providersController'
       i++;
     });
     data.reverse();
-
-    /*
-      var date = new Date(val.date);
-      if (date.getHours() === 0 && date.getMinutes() === 0) {
-        self.days.push({
-          date: val.date,
-          i: i
-        });
-      }
-    */
-
-    // Figure out when it's a new day
-    // number of data points: 288
-    // How many days in that time?
-    /*
-    var seconds = Math.floor((new Date(data[data.length - 1].date) - new Date(data[0].date)) / 1000);
-    var days = Math.round(seconds / (60 * 60 * 24));
-    console.log('days', days);
-    var date = new Date(data[0].date);
-    self.days.push({
-      date: new Date(data[0].date)
-    });
-    self.days.push({
-      date: new Date(data[data.length - 1].date),
-      i: Math.round((data.length)
-    });
-    */
-
-    /*
-    for (i = 0; i < days; i++) {
-      self.days.push({
-        date: angular.copy(date),
-        i: Math.round((data.length / days) * i)
-      });
-      date.setDate(date.getDate() + 1);
-    }
-    */
-
-    console.log(self.days);
     self.heartbeat = data;
     spinnerService.hide('heartbeatlog');
   });
