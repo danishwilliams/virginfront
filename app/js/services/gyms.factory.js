d5a8b525c9545d5b4b5967b1a8c8fed146dd74aa
Roger Saner <rsaner@deloitte.co.za>
2016-05-26 17:43:33 +0200

    Now showing archived clubs too

--- app/js/services/gyms.factory.js
+++ app/js/services/gyms.factory.js
@@ -6,11 +6,10 @@ GymsFactory.$inject = ['Restangular', 'uuid2'];
 
 function GymsFactory(Restangular, uuid2) {
   var self = this;
-  var gymsAll = [];
 
   var gymsFactory = {
     loadGyms: loadGyms,
-    loadAllGyms: loadAllGyms,
+    loadArchivedGyms: loadArchivedGyms,
     loadAvailableGyms: loadAvailableGyms,
     loadGym: loadGym,
     addGym: addGym,
@@ -25,47 +24,28 @@ function GymsFactory(Restangular, uuid2) {
     });
   }
 
-  function loadAllGyms() {
+  function loadArchivedGyms() {
     return Restangular.all('gyms').getList({
-      onlyActiveDevices: false
-    }).then(loadAllGymsComplete);
-
-    function loadAllGymsComplete(data, status, headers, config) {
-      self.gymsAll = data;
-      return self.gymsAll;
-    }
+      enabled: false
+    });
   }
 
   function loadAvailableGyms() {
-    return Restangular.all('gyms/syncinfo').getList().then(loadAvailableGymsComplete);
-
-    function loadAvailableGymsComplete(data, status, headers, config) {
-      return data;
-    }
+    return Restangular.all('gyms/syncinfo').getList();
   }
 
   function loadGym(id) {
-    return Restangular.one('gyms', id).get().then(loadGymComplete);
-
-    function loadGymComplete(data, status, headers, config) {
-      return data;
-    }
+    return Restangular.one('gyms', id).get();
   }
 
   function addGym(gym) {
     gym.Id = uuid2.newuuid().toString();
     gym.Enabled = true;
-    return Restangular.one('gyms', gym.Id).customPUT(gym).then(function (data) {
-      return data;
-    });
+    return Restangular.one('gyms', gym.Id).customPUT(gym);
   }
 
   // Note: to re-enable a gym, just set gym.Enabled = true and put()
   function disableGym(id) {
-    return Restangular.one('gyms/disable', id).post().then(disableGymComplete);
-
-    function disableGymComplete(data, status, headers, config) {
-      return data;
-    }
+    return Restangular.one('gyms/disable', id).post();
   }
 }
